336a97eeb9c73c853f3fe917553851f53e7236fc
Vitaliy Bashkir <vabashkir@icloud.com>
2025-11-06 14:54:30 +0200

    HW6 reworking with remarks HW5 2

--- lib/api.ts
+++ lib/api.ts
@@ -22,10 +22,7 @@ export interface FetchNotesParams {
 
 export interface FetchNotesResponse {
   notes: Note[];
-  page: number;
-  perPage: number;
   totalPages: number;
-  totalItems: number;
 }
 
 export interface CreateNoteBody {
@@ -34,7 +31,6 @@ export interface CreateNoteBody {
   tag: NoteTag;
 }
 
-/** GET /notes ? page & perPage & search */
 export async function fetchNotes(
   params: FetchNotesParams = {},
 ): Promise<FetchNotesResponse> {
@@ -45,19 +41,16 @@ export async function fetchNotes(
   return res.data;
 }
 
-/** POST /notes -> Note */
 export async function createNote(body: CreateNoteBody): Promise<Note> {
   const res = await http.post<Note>("/notes", body);
   return res.data;
 }
 
-/** DELETE /notes/:id -> Note */
 export async function deleteNote(id: string): Promise<Note> {
   const res = await http.delete<Note>(`/notes/${id}`);
   return res.data;
 }
 
-/** GET /notes/:id -> Note */
 export async function fetchNoteById(id: string): Promise<Note> {
   const res = await http.get<Note>(`/notes/${id}`);
   return res.data;
